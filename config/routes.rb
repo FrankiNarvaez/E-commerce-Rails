8db8843f1be74c58932c52fb87ddccb06c8743f1
FrankiNarvaez <frannki03@gmail.com>
2024-12-07 11:26:00 -0500

    Remove route show

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :categories
+  resources :categories, except: :show
   resources :products, path: "/"
 end
