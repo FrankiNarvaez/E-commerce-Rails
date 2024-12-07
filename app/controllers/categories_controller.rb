a2efabd5938be7fb1e920bfd5a0cde92f66528ff
FrankiNarvaez <frannki03@gmail.com>
2024-12-07 10:09:06 -0500

    Method index, new, and create

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,21 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[ edit update destroy ]
   def index
-  end
-
-  def show
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(categories_params)
+
+    if @category.save
+      redirect_to categories_path, notice: t(".created")
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -23,8 +30,10 @@ class CategoriesController < ApplicationController
   private
 
     def categories_params
+      params.require(:category).permit(:name)
     end
 
     def set_category
+      @category = Category.find(params[:id])
     end
 end
