667a941f52c754b3a5b197ddf55b4642d83c2737
FrankiNarvaez <frannki03@gmail.com>
2024-12-06 21:43:43 -0500

    Added method to delete an product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,15 +12,30 @@ class ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to product_path(@product.id), notice: "Your product was created"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @product.update(product_params)
+      redirect_to product_path(@product.id), notice: "Your product was updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @product.destroy
+
+    redirect_to products_path, status: :see_other, notice: "Your product was deleted"
   end
 
   private
@@ -30,5 +45,6 @@ class ProductsController < ApplicationController
     end
 
     def product_params
+      params.require(:product).permit(:title, :description, :price, :stock)
     end
 end
