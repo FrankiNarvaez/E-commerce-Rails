2bbce8d6508c41764df5bad1c171e78d6279e284
FrankiNarvaez <frannki03@gmail.com>
2024-12-06 21:44:09 -0500

    Test to method delete of product

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -23,4 +23,58 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "form"
   end
+
+  test "allow to create a new product" do
+    post products_path, params: {
+      product: {
+        title: "Xiaomi",
+        description: "Poco X3 pro",
+        price: 250,
+        stock: 2
+      }
+    }
+
+    assert_redirected_to product_path(Product.last.id)
+    assert_equal flash[:notice], "Your product was created"
+  end
+
+  test "does not allow to create a new product with empty fields" do
+    post products_path, params: {
+      product: {
+        title: "",
+        description: "Poco X3 pro",
+        price: 250,
+        stock: 2
+      }
+    }
+
+    assert_response :unprocessable_entity
+  end
+
+  test "render an edit product form" do
+    get edit_product_path(products(:iphone))
+
+    assert_response :success
+    assert_select "form"
+  end
+
+  test "allow to update a product" do
+    patch product_path(products(:iphone)), params: {
+      product: {
+        stock: 4
+      }
+    }
+
+    assert_redirected_to product_path(products(:iphone))
+    assert_equal flash[:notice], "Your product was updated"
+  end
+
+  test "can delete products" do
+    assert_difference("Product.count", -1) do
+      delete product_path(products(:iphone))
+    end
+
+    assert_redirected_to products_path
+    assert_equal flash[:notice], "Your product was deleted"
+  end
 end
