10c9fd29800e4a0a3fa30d059f27898549d10ee1
FrankiNarvaez <frannki03@gmail.com>
2024-12-07 11:22:46 -0500

    Added some test to categories

---
+++ test/controllers/categories_controller_test.rb
@@ -0,0 +1,24 @@
+require "test_helper"
+
+class CategoriesControllerTest < ActionDispatch::IntegrationTest
+  test "render a list of categories" do
+    get categories_path
+
+    assert_response :success
+    assert_select ".category", 2
+  end
+
+  test "create a new category" do
+    post categories_path, params: { category: { name: "comics" } }
+
+    assert_redirected_to categories_path
+    assert_equal flash[:notice], "Your category was created"
+  end
+
+  test "edit a category" do
+    patch category_path(categories(:technology)), params: { category: { name: "tech" } }
+
+    assert_redirected_to categories_path
+    assert_equal flash[:notice], "Your category was updated"
+  end
+end
