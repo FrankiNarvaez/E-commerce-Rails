c56d00c4ab55be7e39789b2bc5cf401bc4c0be4c
FrankiNarvaez <frannki03@gmail.com>
2024-12-08 16:19:38 -0500

    User register

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true, length: { in: 3..50 }, format: { with: /\A[a-zA-Z]+\z/, message: :invalid }
+  validates :email, presence: true, uniqueness: true, format: {
+      with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
+      message: :invalid
+    }
+  validates :password_digest, length: { minimum: 8 }
 end
