88350561e2b82cb9e291d3e41b11bbe2c31749a8
FrankiNarvaez <frannki03@gmail.com>
2024-12-08 11:25:22 -0500

    Infinite pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,6 +23,9 @@ class ProductsController < ApplicationController
     order_by = Product::ORDER_BY.fetch(params[:order_by]&.to_sym, Product::ORDER_BY[:newest])
 
     @products = @products.order(order_by).load_async
+
+    @pagy, @products = pagy_countless(@products, items: 12)
+    pp @pagy, @pagy.page, @pagy&.next
   end
 
   def show
