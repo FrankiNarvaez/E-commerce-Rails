f4d32d73fa4dddb7372aaff3a56d0b066d52ed4e
FrankiNarvaez <frannki03@gmail.com>
2024-12-08 13:32:47 -0500

    Added user

---
+++ db/migrate/20241208173724_add_null_false_to_categories_fields.rb
@@ -0,0 +1,5 @@
+class AddNullFalseToCategoriesFields < ActiveRecord::Migration[8.0]
+  def change
+    change_column_null :categories, :name, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_07_164253) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_08_182920) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -43,7 +43,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_07_164253) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -57,6 +57,15 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_07_164253) do
     t.datetime "updated_at", null: false
     t.bigint "category_id", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "name", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
