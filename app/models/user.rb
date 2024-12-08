186574cd50edb8fc4d68be2681f7f86a4842c71d
FrankiNarvaez <frannki03@gmail.com>
2024-12-08 17:03:39 -0500

    Added column username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :name, presence: true, length: { in: 3..50 }, format: { with: /\A[a-zA-Z]+\z/, message: :invalid }
+  validates :name, presence: true, length: { in: 3..50 }, format: { with: /\A[a-zA-Z\s]+\z/, message: :invalid }
+  validates :username, presence: true, length: { in: 3..15 }, format: { with: /\A[a-z0-9A-Z]+\z/, message: :invalid }
   validates :email, presence: true, uniqueness: true, format: {
       with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
       message: :invalid
     }
-  validates :password_digest, length: { minimum: 8 }
+  validates :password, length: { minimum: 6 }
 end
---
+++ db/migrate/20241208214559_add_username_field_to_user.rb
@@ -0,0 +1,6 @@
+class AddUsernameFieldToUser < ActiveRecord::Migration[8.0]
+  def change
+    add_column :users, :username, :string, null: false
+    add_index :users, :username, unique: true
+  end
+end
