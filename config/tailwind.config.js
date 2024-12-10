d74fb80ea3ed21f63d62c426fde45be1641333b5
FrankiNarvaez <frannki03@gmail.com>
2024-12-10 13:46:34 -0500

    Added styles to header and product index

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'main-color': '#4F39F6'
+      },
     },
   },
   plugins: [
