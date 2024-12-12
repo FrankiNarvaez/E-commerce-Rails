e906fbd5a81f4d2e9071ff1fd938cfd78fd7e6c7
FrankiNarvaez <frannki03@gmail.com>
2024-12-11 21:51:42 -0500

    Changed icon menu

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,12 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["items", "open_menu", "close_menu"]
+  static targets = ["items"]
 
-  toggle () {
+  toggle() {
     this.itemsTarget.classList.toggle('-translate-y-48');
     this.itemsTarget.classList.toggle('h-0');
-    this.open_menuTarget.classList.toggle('hidden');
-    this.close_menuTarget.classList.toggle('hidden');
   }
 }
