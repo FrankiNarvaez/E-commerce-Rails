d43c1a485b56b834ee3a259fb7ea8548eb7ec74b
FrankiNarvaez <frannki03@gmail.com>
2024-12-14 18:36:02 -0500

    Added welcome message

--- app/controllers/authentication/users_controller.rb
+++ app/controllers/authentication/users_controller.rb
@@ -7,8 +7,11 @@ class Authentication::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    @user.country = FetchCountryService.new(request.remote_ip).perform
+    @user.city = FetchCityService.new(request.remote_ip).perform
 
     if @user.save
+      UserMailer.with(user: @user).welcome.deliver_later
       session[:user_id] = @user.id
       redirect_to products_path, notice: t(".created")
     else
