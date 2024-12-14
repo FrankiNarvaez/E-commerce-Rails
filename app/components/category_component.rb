7be68b7854a52c52737c2c8db258c9de4ce56381
FrankiNarvaez <frannki03@gmail.com>
2024-12-14 15:29:37 -0500

    Refactored

--- app/components/category_component.rb
+++ app/components/category_component.rb
@@ -1,21 +1,23 @@
 # frozen_string_literal: true
 
 class CategoryComponent < ViewComponent::Base
+  attr_reader :category
+
   def initialize(category: nil)
     @category = category
   end
 
   def title
-    @category ? @category.name : t(".all")
+    category ? category.name : t(".all")
   end
 
   def link
-    @category ? products_path(category_id: @category.id) : products_path
+    category ? products_path(category_id: category.id) : products_path
   end
 
   def active?
-    return true if !@category && !params[:category_id]
-    @category&.id == params[:category_id].to_i
+    return true if !category && !params[:category_id]
+    category&.id == params[:category_id].to_i
   end
 
   def background
